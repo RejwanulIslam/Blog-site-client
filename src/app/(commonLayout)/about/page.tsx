9872e8068b19b59915d045af82d3c706dd61913d
RejwanulIslam <174478473+RejwanulIslam@users.noreply.github.com>
2026-01-17 11:33:17 +0600

    router group implement dashboard layour and commonlayout

--- src/app/(commonLayout)/about/page.tsx
+++ src/app/(commonLayout)/about/page.tsx
@@ -1,7 +1,7 @@
-
+export const dynamic='force-dynamic' 
 export default async function AboutPage() {
   await new Promise(resolv=> setTimeout(resolv,4000))
-  throw new Error("halllooo")
+  // throw new Error("halllooo")
   return (
     <div>AboutPage</div>
   )
--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -73,6 +73,10 @@ const Navbar = ({
       title: "About",
       url: "/about",
     },
+    {
+      title: "Dashboard",
+      url: "/dashboard",
+    },
 
   ],
   auth = {
